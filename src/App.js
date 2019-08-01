03f7325caef94e4a72e35e552de59d1e2321c009
peterfchris <peter.f.chris@gmail.com>
2019-08-01 10:52:55 -0600

    Navbar setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
+import Navbar from './Components/Layout/Navbar'
 
 class App extends Component {
   render(){
     return (
       <div>
-        <h1>Hello from React</h1>
+        <Navbar />
       </div>
     );
   }
