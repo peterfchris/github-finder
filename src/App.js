e678f7be76f6c816749459f74321cd050d989421
peterfchris <peter.f.chris@gmail.com>
2019-08-06 18:09:25 -0600

    feat: UserItem component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './Components/Layout/Navbar'
+import UserItem from './Components/Users/UserItem'
 
 class App extends Component {
   render(){
     return (
       <div>
         <Navbar />
+        <UserItem />
       </div>
     );
   }
