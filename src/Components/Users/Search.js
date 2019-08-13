0f6402302d907a6dd88eba7cf3829954f64e5f3d
peterfchris <peter.f.chris@gmail.com>
2019-08-13 13:45:11 -0600

    feat: search bar component added

---
+++ src/Components/Users/Search.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export class Search extends Component {
+    state = {
+        text: ''
+    }
+
+    onChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmit = e => {
+        e.preventDefault()
+        console.log('this.state.text', this.state.text)
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.onSubmit} className='form'>
+                    <input type='text' name='text' placeholder='Search Users...' value={this.state.text} onChange={this.onChange} />
+                    <input type='submit' className='btn btn-dark btn-block' value='Search' />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Search
